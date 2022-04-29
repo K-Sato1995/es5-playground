4c65f28b8358f2d910425864494f9ec73cc1449c
K-Sato <katsuki_sato0130@icloud.com>
2022-04-30 07:31:32 +0900

    Forbit es6funcs

--- src/index.js
+++ src/index.js
@@ -6,9 +6,54 @@ let code = `var msg = "Hello World"\nconsole.log(msg)`;
 
 const linter = new Linter();
 
+linter.defineRule("no-es6-funcs", {
+  meta: {
+    docs: {
+      recommended: false
+    }
+  },
+  create(context) {
+    return {
+      CallExpression(node) {
+        if(!node.callee || !node.callee.property) {
+          return;
+        }
+        const functionName = node.callee.property.name;
+        const es6ArrayFunctions = [
+          'find',
+          'findIndex',
+          'copyWithin',
+          'values',
+          'fill'
+        ];
+        const es6StringFunctions = [
+          'startsWith',
+          'endsWith',
+          'includes',
+          'repeat'
+        ];
+        const es6Functions = [].concat(
+          es6ArrayFunctions,
+          es6StringFunctions
+        );
+        if (es6Functions.indexOf(functionName) > -1) {
+          context.report({
+            node: node.callee.property,
+            message: 'ES6 methods not allowed: ' + functionName
+          });
+        }
+      }
+    };
+  }
+})
+
+
 const lint = (code) => {
   const rules = linter.getRules();
 
+  console.log(rules)
+
+  
   // Set reccomended rules
   const ruleConfig = [...rules.entries()].reduce((result, [ruleId, rule]) => {
     if (rule.meta.docs.recommended) {
@@ -17,18 +62,18 @@ const lint = (code) => {
     return result;
 }, {})
 
-  // Verify a code.
-  // See the official document of the Linter class.
   const messages = linter.verify(
     code,
     {
       parserOptions: {
-        ecmaVersion: 3
+        ecmaVersion: 5,
       },
       rules: {
         ...ruleConfig,
-        'no-undef': 0
-      }
+        'no-undef': 0,
+        'unusedVar': 0,
+        "no-es6-funcs": 2
+      },
     },
   );
 
