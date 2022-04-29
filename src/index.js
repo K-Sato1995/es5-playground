ce68f02d43ed8a5d40d7001712d761f4aa158ce8
K-Sato <katsuki_sato0130@icloud.com>
2022-04-30 06:12:53 +0900

    Set recommended rules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import CodeFlask from "codeflask";
+// Copied the versions from here: https://github.com/node-red/nrlint/blob/master/package.json#L50
 import Linter from "eslint4b";
 
+let code = `var msg = "Hello World"\nconsole.log(msg)`;
 
+const linter = new Linter();
 
-let code = `var msg = "Hello World"\nconsole.log(test)`;
+const lint = (code) => {
+  const rules = linter.getRules();
+
+  // Set reccomended rules
+  const ruleConfig = [...rules.entries()].reduce((result, [ruleId, rule]) => {
+    if (rule.meta.docs.recommended) {
+        result[ruleId] = 2;
+    }
+    return result;
+}, {})
+
+  // Verify a code.
+  // See the official document of the Linter class.
+  const messages = linter.verify(
+    code,
+    {
+      parserOptions: {
+        ecmaVersion: 3
+      },
+      rules: {
+        ...ruleConfig,
+        'no-undef': 0
+      }
+    },
+  );
+
+  return messages;
+};
+
 const flask = new CodeFlask("#editor", {
   language: "js",
   lineNumbers: true,
@@ -12,47 +43,8 @@ const flask = new CodeFlask("#editor", {
 flask.updateCode(code);
 
 flask.onUpdate((code) => {
-  console.log(code);
+  const messages = lint(code);
+  console.log(messages);
+
+  document.getElementById("lint-errors").innerHTML = (`<pre>${JSON.stringify(messages)}</pre>`);
 });
-
-const linter = new Linter();
-const rules = linter.getRules();
-
-
-// Verify a code.
-// See the official document of the Linter class.
-const messages = linter.verify(
-    "var foo = 0",
-    {
-        rules: {
-            semi: "error"
-        }
-    },
-    { filename: "foo.js" }
-);
-
-// const lint = () => {
-//   try {
-//     const { messages, output } = linter.verifyAndFix(
-//       this.state.text,
-//       this.state.options,
-//       { fix: this.state.fix }
-//     );
-//     let fatalMessage;
-
-//     if (messages && messages.length > 0 && messages[0].fatal) {
-//       fatalMessage = messages[0];
-//     }
-//     return {
-//       messages,
-//       output,
-//       fatalMessage,
-//     };
-//   } catch (error) {
-//     return {
-//       messages: [],
-//       output: this.state.text,
-//       error,
-//     };
-//   }
-// };
