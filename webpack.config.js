93727e9058a32d1cd051b1743cd4aa75377732a8
K-Sato <katsuki_sato0130@icloud.com>
2022-04-29 20:21:43 +0900

    Use webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+  
+const path = require("path");
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+  },
+  mode: 'development'
+};
